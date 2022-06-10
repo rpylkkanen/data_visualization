4a883ba8dfadd0c70ef19727769a511aaa5b429f
rpylkkanen <robert.pylkkanen@gmail.com>
2022-06-10 18:13:56 +0300

    colors

--- __init__.py
+++ __init__.py
@@ -1,3 +1,25 @@
 from .matplotlib_exact import Alignment
 from .matplotlib_style import colors
 from .matplotlib_extension import *
+
+color_index = 5
+
+red = colors['red'][color_index]
+pink = colors['pink'][color_index]
+purple = colors['purple'][color_index]
+deep_purple = colors['deep purple'][color_index]
+indigo = colors['indigo'][color_index]
+blue = colors['blue'][color_index]
+light_blue = colors['light blue'][color_index]
+cyan = colors['cyan'][color_index]
+teal = colors['teal'][color_index]
+green = colors['green'][color_index]
+light_green = colors['light green'][color_index]
+lime = colors['lime'][color_index]
+yellow = colors['yellow'][color_index]
+amber = colors['amber'][color_index]
+orange = colors['orange'][color_index]
+deep_orange = colors['deep orange'][color_index]
+brown = colors['brown'][color_index]
+grey = colors['grey'][color_index]
+blue_grey = colors['blue grey'][color_index]
