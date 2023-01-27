6a10204adc269ce3964499901ea564611d4048cb
rpylkkanen <robert.pylkkanen@gmail.com>
2023-01-27 12:34:31 +0200

    smoothing and contrast

--- __init__.py
+++ __init__.py
@@ -15,6 +15,43 @@ from .matplotlib_exact import Alignment
 from .matplotlib_style import colors
 from .matplotlib_extension import *
 
+## Image adjustments
+
+import cv2
+from math import factorial
+
+def adjust_contrast(file, limit=1.0, grid_size=(8, 8)):
+
+  # Load the image in greyscale
+  img = cv2.imread(file,0)
+
+  # create a CLAHE object (Arguments are optional).
+  clahe = cv2.createCLAHE(clipLimit=limit, tileGridSize=grid_size)
+  out = clahe.apply(img)
+
+  return out
+
+def savitzky_golay(y, window_size=21, order=2, deriv=0, rate=1):
+  window_size = np.abs(np.int(window_size))
+  order = np.abs(np.int(order))
+  if window_size % 2 != 1 or window_size < 1:
+      raise TypeError("window_size size must be a positive odd number")
+  if window_size < order + 2:
+      raise TypeError("window_size is too small for the polynomials order")
+  order_range = range(order+1)
+  half_window = (window_size -1) // 2
+  # precompute coefficients
+  b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+  m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+  # pad the signal at the extremes with
+  # values taken from the signal itself
+  firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+  lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+  y = np.concatenate((firstvals, y, lastvals))
+  return np.convolve( m[::-1], y, mode='valid')
+
+def smooth(y, kwargs={}):
+  return savitzky_golay(y, **kwargs)
 
 ### Color shortcuts
 
